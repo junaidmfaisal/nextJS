01bc3e80eae1ec7c311a589b46af456a3dfef0c5
JUNAID M FAISAL <64261548+junaidmfaisal@users.noreply.github.com>
2025-08-28 21:55:53 +0530

    final before hosting

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,6 +3,7 @@ import Service2 from '@/components/Service2'
 import Service3 from '@/components/Service3'
 import Service4 from '@/components/Service4'
 import Service5 from '@/components/Services5'
+import Service6 from '@/components/Services6'
 import React from 'react'
 
 function page() {
@@ -12,7 +13,8 @@ function page() {
       <Service2/>
       <Service3/>
       <Service4/>
-      <Service5/>
+      {/* <Service5/>
+      <Service6/> */}
     </div>
   )
 }
